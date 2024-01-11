42246117a88535b375776b6cbf68c6dc845e1a78
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-10 20:45:03 -0800

    Month and years logic works

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-const currentYearDiv = document.querySelector(".current-year");
-const currentYear = new Date().getFullYear();
-currentYearDiv.textContent = currentYear;
+const nextButton = document.querySelector(".next img");
+const prevButton = document.querySelector(".previous img");
+let monthAndYear = document.querySelector(".month-and-year");
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let currentMonth = months[new Date().getMonth()];
+let currentYear = new Date().getFullYear();
+let year = currentYear;
+let month = currentMonth;
+monthAndYear.textContent = currentMonth + " " + currentYear;
 
-const currentMonthDiv = document.querySelector(".current-month");
-const currentMonth = months[new Date().getMonth()];
-currentMonthDiv.textContent = currentMonth;
+nextButton.addEventListener("click", () => {
+  nextMonth();
+});
+
+prevButton.addEventListener("click", () => {
+  prevMonth();
+});
+
+function nextMonth() {
+  if (month === "December") {
+    month = "January";
+    year++;
+  } else {
+    month = months[months.indexOf(month) + 1];
+  }
+
+  monthAndYear.textContent = month + " " + year;
+}
+
+function prevMonth() {
+  if (month === "January") {
+    month = "December";
+    year--;
+  } else {
+    month = months[months.indexOf(month) - 1];
+  }
+
+  monthAndYear.textContent = month + " " + year;
+}
