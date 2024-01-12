67b149b716a7b2830cd383d71d27fe58d83ff6e3
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-11 19:15:25 -0800

    Logic works for adding day numbers to calendar

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const nextButton = document.querySelector(".next img");
 const prevButton = document.querySelector(".previous img");
+const calendarBody = document.querySelector(".calendar-body");
 let monthAndYear = document.querySelector(".month-and-year");
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+let currentDay = new Date().getDate();
 let currentMonth = months[new Date().getMonth()];
 let currentYear = new Date().getFullYear();
+
 let year = currentYear;
 let month = currentMonth;
+
+console.log("Current day: " + currentDay);
+
 monthAndYear.textContent = currentMonth + " " + currentYear;
+drawCalendarBody(currentMonth, currentYear);
 
 window.addEventListener("keyup", (event) => {
   if (event.ctrlKey && event.key === "ArrowRight") {
@@ -35,6 +44,7 @@ function nextMonth() {
     month = months[months.indexOf(month) + 1];
   }
 
+  drawCalendarBody(month, year);
   monthAndYear.textContent = month + " " + year;
 }
 
@@ -46,5 +56,49 @@ function prevMonth() {
     month = months[months.indexOf(month) - 1];
   }
 
+  drawCalendarBody(month, year);
   monthAndYear.textContent = month + " " + year;
+}
+
+function drawCalendarBody(month, year) {
+  const firstDayNum = new Date(year, months.indexOf(month), 1).getDay();
+  const firstDay = weekdays[firstDayNum];
+  const daysInMonth = 32 - new Date(year, months.indexOf(month), 32).getDate();
+
+  calendarBody.innerHTML = ""; // Clear calendar body
+
+  for (let i = 0; i < 42; i++) {
+    const div = document.createElement("div");
+    div.classList.add("day");
+
+    // Add empty class to days before and after current month
+
+    if (i < firstDayNum || i >= daysInMonth + firstDayNum) {
+      div.classList.add("empty");
+    }
+
+    // Add today class to current day
+
+    if (i === currentDay + firstDayNum - 1 && month === currentMonth && year === currentYear) {
+      div.classList.add("today");
+    }
+
+    // Add number to day div
+
+    if (i >= firstDayNum && i < daysInMonth + firstDayNum) {
+      div.textContent = i - firstDayNum + 1;
+    }
+
+    // Add number to day div for next month
+
+    if (i >= daysInMonth + firstDayNum) {
+      div.textContent = i - daysInMonth - firstDayNum + 1;
+    }
+
+    calendarBody.appendChild(div);
+  }
+
+  console.log("Month: " + month);
+  console.log("First day of the month is: " + firstDay);
+  console.log("Days in month: " + daysInMonth);
 }
